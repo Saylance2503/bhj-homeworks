054dda5edba1ab00c8d9bd5d7cc5626085114420
Saylance2503 <yurik_05@inbox.ru>
2023-06-11 23:09:15 +0400

    feat: solution progressbar

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,30 +1,38 @@
 const fileInput = document.getElementById('fileInput');
 const progress = document.getElementById('progress');
 
+const xhr = new XMLHttpRequest();
+
+const formData = new FormData();
+
 fileInput.addEventListener('change', (event) => {
   const file = event.target.files[0];
 
   if (file) {
-    const formData = new FormData();
     formData.append('file', file);
 
-    fetch('https://students.netoservices.ru/nestjs-backend/upload', {
-      method: 'POST',
-      body: formData,
-      onUploadProgress: (progressEvent) => {
-        const progressPercentage = Math.round((progressEvent.loaded / progressEvent.total) * 100);
+    xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload', true);
+
+    xhr.upload.addEventListener('progress', (event) => {
+      if (event.lengthComputable) {
+        const progressPercentage = (event.loaded / event.total) * 100;
         progress.value = progressPercentage / 100;
-      },
-    })
-      .then((response) => {
-        if (response.ok) {
-          console.log('Файл успешно загружен');
-        } else {
-          console.error('Ошибка при загрузке файла');
-        }
-      })
-      .catch((error) => {
-        console.error('Ошибка при выполнении запроса', error);
-      });
+      }
+    });
+
+    xhr.onload = function () {
+      if (xhr.status != 200) {
+        console.error('Ошибка при загрузке файла');
+        return;
+      } else {
+        console.log('Файл успешно загружен');
+      }
+    };
+
+    xhr.onerror = function () {
+      console.error('Ошибка при выполнении запроса');
+    };
+
+    xhr.send(formData);
   }
 });
